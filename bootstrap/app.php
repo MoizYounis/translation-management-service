933506d8e3c8d8b1f9f17900f76cc7d377e66785
Muhammad Moiz Younis <muhammadmoizyounis6547@gmail.com>
2025-06-14 16:17:07 +0500

    Exception handeling added

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,17 +3,38 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Auth\Access\AuthorizationException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        api: __DIR__.'/../routes/api.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: __DIR__ . '/../routes/web.php',
+        api: __DIR__ . '/../routes/api.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
         //
     })
     ->withExceptions(function (Exceptions $exceptions): void {
-        //
+        $exceptions->renderable(function (AuthorizationException $e) {
+            return response()->json([
+                'status' => false,
+                'message' => 'Unauthenticated',
+            ], 401);
+        });
+
+        $exceptions->renderable(function (NotFoundHttpException $e) {
+            return response()->json([
+                'status' => false,
+                'message' => 'Route Not Found',
+            ], 404);
+        });
+
+        $exceptions->renderable(function (\Throwable $e) {
+            return response()->json([
+                'status' => false,
+                'message' => $e->getMessage(),
+            ]);
+        });
     })->create();
